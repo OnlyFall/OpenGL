5fe912bd971957be95b9db6373880d70da537e4d
onlyFall <kwb010712@naver.com>
2018-10-13 02:25:40 +0900

    19완성
    !

--- Chapter - 1/Project8/1 - 18.cpp
+++ Chapter - 1/Project8/1 - 18.cpp
@@ -16,6 +16,25 @@ typedef struct Translate_pos {
 	float degree;
 };
 
+struct Camera {
+	int xRad = -30;
+	int yRad;
+	int zRad;
+
+	float x;
+	float y;
+	float z;
+
+	BOOL xBOOL = FALSE;
+	int xDir;
+	BOOL yBOOL = FALSE;
+	int yDir;
+	BOOL zBOOL = FALSE;
+	int zDir;
+};
+
+static Camera camera;
+
 
 Translate_pos camera;
 struct position {
@@ -485,3 +504,279 @@ void Mouse(int button, int state, int x, int y)
 {
 	glutPostRedisplay();
 }
+
+//#include <GL/freeglut.h> 
+//#include <random>
+//#include <time.h>
+//#include <math.h>
+//GLvoid Reshape(int w, int h);
+//
+////해상도 설정
+//#define WideSize 800
+//#define HighSize 600
+//#define Z_Size 400
+//
+////그리기 제어
+//GLvoid drawScene(GLvoid);
+//BOOL draw_Act_On;
+//
+////타이머 제어
+//void Timerfunction(int value);
+//BOOL Time_Act_On; //타이머 활성화 여부
+//int Time_count; //타이머 카운트
+//
+//				//마우스 제어
+//void Mouse(int button, int state, int x, int y);
+//POINT ms_click; //마우스 좌표
+//BOOL Mouse_Act_On; //마우스 활성화 여부
+//
+//				   //키보드 제어
+//void Keyboard(unsigned char key, int x, int y);
+//BOOL Keyboard_Act_On; //키보드 활성화 여부
+//
+//					  //색 선정
+//typedef struct Color
+//{
+//	int R;
+//	int G;
+//	int B;
+//};
+//typedef struct Translate_pos {
+//	GLdouble x;
+//	GLdouble y;
+//	GLdouble z;
+//	float degree;
+//};
+//
+//#define PT 20//도형 갯수 설정
+//#define PI 3.141592 //파이
+//typedef struct Shape
+//{
+//	Color cl;//색상
+//	Translate_pos pos;
+//	Translate_pos move;
+//	Translate_pos scale;
+//	Translate_pos rot;
+//
+//	int size;
+//	int select;
+//	int height;
+//	int slice;
+//	int stacks;
+//
+//	BOOL any;
+//
+//};
+//
+//static float see = 0;
+//int change_count;
+//int select_count;
+//int st_help;
+//BOOL Save = false;
+//BOOL ani = FALSE;
+//BOOL Look = FALSE;
+//
+//Shape camera;
+//Shape tra;
+//int rot_count;
+//int rot_command;
+//
+//Shape shape[2];
+//Shape view;
+//void SetupRC()
+//{
+//	//초기화
+//	glTranslated(WideSize / 2, HighSize / 2, Z_Size / 2);
+//}
+//void main(int argc, char *argv[]) {
+//	//초기화
+//
+//	camera.rot.x = 0;
+//	camera.rot.y = 1;
+//	camera.rot.z = 1;
+//
+//	shape[0].pos.x = 100;
+//	shape[1].pos.x = 100;
+//	glutInit(&argc, argv);
+//	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+//	glutInitWindowPosition(100, 100);//윈도우 띄우기 좌표
+//	glutInitWindowSize(WideSize, HighSize); //윈도우 띄우기 크기
+//	glutCreateWindow("ex16");
+//	// - 랜덤으로 시작 도형 설정하기
+//	//도형 그리기
+//	//SetupRC();
+//
+//	glutDisplayFunc(drawScene);
+//	glutReshapeFunc(Reshape);
+//	glutMouseFunc(Mouse);
+//	glutKeyboardFunc(Keyboard);
+//	glutTimerFunc(100, Timerfunction, 1);
+//	srand(time(NULL));
+//	glutMainLoop();
+//}
+//
+//GLvoid drawScene(GLvoid)
+//{
+//	glEnable(GL_DEPTH_TEST);
+//	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//
+//	GLdouble x_rotate[16] = {
+//		1, 0, 0, 0,
+//		0, cos(camera.rot.degree), -sin(camera.rot.degree), 0,
+//		0, sin(camera.rot.degree), cos(camera.rot.degree), 0,
+//		0,0,0,1 };
+//
+//	GLdouble y_rotate[16] = {
+//	};
+//
+//	glLoadMatrixd(x_rotate);
+//
+//	glPushMatrix(); {
+//
+//			
+//		glLineWidth(2);
+//		glColor3f((float)255 / 255, (float)255 / 255, (float)255 / 255);
+//		glMatrixMode(GL_MODELVIEW);
+//		//좌표축 그리기
+//		glPushMatrix(); {
+//
+//			glTranslated(0, -15, 0);
+//			glScalef(1, 0.001, 1);
+//			glutSolidCube(200);//발판 만들기
+//			glPopMatrix();
+//		}
+//			
+//		//가운데 막대 그리기
+//		glColor3f((float)255 / 255, (float)0 / 255, (float)0 / 255);
+//
+//		for (int i = 0; i < 3; i++) {
+//			glPushMatrix();
+//			if (i == 0) {
+//				glColor3f((float)255 / 255, (float)0 / 255, (float)0 / 255);
+//				glRotated(90, 1, 0, 0);
+//			}
+//			else if (i == 1) {
+//				glColor3f((float)0 / 255, (float)255 / 255, (float)0 / 255);
+//				glRotated(90, 0, 1, 0);
+//			}
+//			else if (i == 2) {
+//				glColor3f((float)0 / 255, (float)0 / 255, (float)255 / 255);
+//				glRotated(90, 0, 0, 1);
+//			}
+//			glScalef(1, 0.1, 0.1);
+//			glutSolidCube(40);
+//			glPopMatrix();
+//		}//좌표계 그리기
+//
+//		 //glRotatef(Time_count, 0, 1, 0);
+//	}
+//	glPopMatrix();
+//
+//	glutSwapBuffers();
+//}
+//
+//void Mouse(int button, int state, int x, int y) {
+//	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+//	{
+//
+//	}
+//	glutPostRedisplay();
+//
+//}
+//void Timerfunction(int value) {
+//
+//	glutPostRedisplay(); //타이머에 넣는다.
+//	glutTimerFunc(100, Timerfunction, 1); //타이머 다시 출력
+//
+//}
+//void Keyboard(unsigned char key, int x, int y) {
+//	switch (key)
+//	{
+//
+//		//------------------카메라------------------------
+//	case 'x':
+//		camera.rot.degree -= 0.1;
+//		camera.rot.z = cos((3.141592 / 180.f) * camera.rot.degree) * 100;
+//		camera.rot.y = sin((3.141592 / 180.f) * camera.rot.degree) * 100;
+//		break;
+//
+//	case 'X':
+//		camera.rot.degree += 1;
+//		camera.rot.z = cos((3.141592 / 180.f) * camera.rot.degree) * 100;
+//		camera.rot.y = sin((3.141592 / 180.f) * camera.rot.degree) * 100;
+//		break;
+//
+//	case 'y':
+//		camera.rot.degree -= 1;
+//		camera.rot.x = cos(PI *camera.rot.degree * 10 / 90);
+//		camera.rot.z = sin(PI * camera.rot.degree * 10 / 90);
+//		break;
+//
+//	case 'Y':
+//		camera.rot.degree += 1;
+//		camera.rot.x = cos(PI *camera.rot.degree * 10 / 90);
+//		camera.rot.z = sin(PI * camera.rot.degree * 10 / 90);
+//		break;
+//
+//	case 'z':
+//		camera.rot.degree -= 1;
+//		camera.rot.x = cos(PI *camera.rot.degree * 10 / 90);
+//		camera.rot.y = sin(PI * camera.rot.degree * 10 / 90);
+//		break;
+//
+//	case 'Z':
+//		camera.rot.degree += 1;
+//		camera.rot.x = cos(PI *camera.rot.degree * 10 / 90);
+//		camera.rot.y = sin(PI * camera.rot.degree * 10 / 90);
+//		break;
+//
+//	case 'w':
+//		camera.rot.x += 1;
+//		break;
+//
+//	case 's':
+//		camera.rot.x -= 1;
+//		break;
+//
+//	case 'd':
+//		camera.rot.y += 1;
+//		break;
+//
+//	case 'a':
+//		camera.rot.y -= 1;
+//		break;
+//
+//	case '+':
+//		camera.rot.z += 1;
+//		break;
+//
+//	case '-':
+//		camera.rot.degree -= 1;
+//		break;
+//
+//	case 'i':
+//		camera.rot.x = 0;
+//		camera.rot.y = 1;
+//		camera.rot.z = 1;
+//		break;
+//		//-----------카메라 끝 --------
+//	default:
+//		;
+//		break;
+//	}
+//	glutPostRedisplay();
+//
+//}
+//
+//GLvoid Reshape(int w, int h)
+//{
+//	glViewport(0, 0, w, h);
+//
+//	glMatrixMode(GL_PROJECTION);
+//	glLoadIdentity();
+//	gluPerspective(45, WideSize / HighSize, 1, Z_Size); //윈도우를 초기화 하는 함수입니다!
+//	glTranslatef(0, 0, -300);
+//	glMatrixMode(GL_MODELVIEW);
+//
+//}
--- Chapter - 1/Project8/1 - 19.cpp
+++ Chapter - 1/Project8/1 - 19.cpp
@@ -16,14 +16,37 @@ typedef struct Translate_pos {
 	float degree;
 };
 
+struct Spe {
+	float x;
+	float y;
+	float z;
+};
 
-Translate_pos camera;
+struct Camera {
+	int xRad = 30;
+	int yRad;
+	int zRad;
+
+	float x;
+	float y;
+	float z;
+
+	BOOL xBOOL = FALSE;
+	int xDir;
+	BOOL yBOOL = FALSE;
+	int yDir;
+	BOOL zBOOL = FALSE;
+	int zDir;
+};
+
+static Camera camera;
+
+static Spe sp;
 
 struct position {
 	int x;
 	int y;
 	int z;
-
 	float r;
 	float g;
 	float b;
@@ -34,6 +57,7 @@ struct position {
 };
 
 static position type[20];
+static position rad;
 
 static float R;
 static float G;
@@ -63,10 +87,13 @@ void main(int agrc, char *argv[]) { // 윈도우 초기화 및 생성
 	glutTimerFunc(100, TimerFunction, 1); // 타이머 콜백 함수
 	glutMainLoop(); // 이벤트 루프 실행하기
 }
-static int rad;
-static int rad2;
-static int rad3;
-static int rad4;
+
+static BOOL xMove = FALSE;
+static BOOL yMove = FALSE;
+static BOOL zMove = FALSE;
+static int xDir = 0;
+static int yDir = 0;
+static int zDir = 0;
 
 static int xB, yB, zB;
 
@@ -77,19 +104,24 @@ GLvoid drawScene(GLvoid)
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
+	
 	glPushMatrix();
-	gluLookAt(
-		camera.x, camera.y, camera.z,  //위5 eye
-		0, 0, 0, //방향 center
-		0, 1, 0); //위쪽방향(건들 ㄴㄴ) up
+	glTranslatef(camera.x, camera.y, camera.z);
+
+
+	glRotatef(camera.xRad, 1, 0, 0);
+	glRotatef(camera.yRad, 0, 1, 0);
+	glRotatef(camera.zRad, 0, 0, 1);
+
+	
 	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 	{
 		glPushMatrix();
-		glRotatef(xB, 1, 0, 0);
-		glRotatef(yB, 0, 1, 0);
-		glRotatef(zB, 0, 0, 1);
+		//glRotatef(xB, 1, 0, 0);
+		//glRotatef(yB, 0, 1, 0);
+		//glRotatef(zB, 0, 0, 1);
 
-		glRotatef(30, 1, 0, 0);
+	//	glRotatef(30, 1, 0, 0);
 		glScalef(1, 0.1, 1);
 		glutSolidCube(400);
 		glPopMatrix();
@@ -103,7 +135,7 @@ GLvoid drawScene(GLvoid)
 		glRotatef(zB, 0, 0, 1);
 
 		glTranslatef(25, 25, 0);
-		glRotatef(30, 1, 0, 0);
+	//	glRotatef(30, 1, 0, 0);
 		glScalef(1, 0.1, 0.1);
 		glutSolidCube(50);
 		glPopMatrix();
@@ -117,7 +149,7 @@ GLvoid drawScene(GLvoid)
 		glRotatef(zB, 0, 0, 1);
 
 		glTranslatef(0, 40, 10);
-		glRotatef(30, 1, 0, 0);
+	//	glRotatef(30, 1, 0, 0);
 		glScalef(0.1, 1, 0.1);
 		glutSolidCube(50);
 		glPopMatrix();
@@ -130,12 +162,39 @@ GLvoid drawScene(GLvoid)
 		glRotatef(yB, 0, 1, 0);
 		glRotatef(zB, 0, 0, 1);
 
-		glTranslatef(0, 15, 25);
-		glRotatef(30, 1, 0, 0);
+		glTranslatef(0, 30, 25);
+	//	glRotatef(30, 1, 0, 0);
 		glScalef(0.1, 0.1, 1);
 		glutSolidCube(50);
 		glPopMatrix();
 	}
+
+
+	{
+		glPushMatrix();
+
+		if (xMove == TRUE) {
+			glTranslatef(sp.x, 80, sp.z);
+			glRotatef(rad.radian, 0, 0, 1);
+		}
+		else if (zMove == TRUE) {
+			glTranslatef(sp.x, 80, sp.z);
+			glRotatef(rad.radian, 1, 0, 0);
+		}
+		else {
+			glTranslatef(sp.x, 80, sp.z);
+			glRotatef(rad.radian, 0, 1, 0);
+		}
+		glutWireSphere(50, 20, 20);
+
+		glPopMatrix();
+	}
+
+
+
+
+
+
 	glPopMatrix();
 
 	
@@ -167,6 +226,63 @@ GLvoid Reshape(int w, int h)
 void TimerFunction(int value)
 {
 	glutPostRedisplay();
+
+
+	if (xMove == TRUE) {
+		if (xDir == 1) {
+			if(sp.x < 160)
+				sp.x += 1;
+			rad.radian += 10;
+		}
+
+		else if (xDir == 2) {
+			if (sp.x > -160)
+				sp.x -= 1;
+			rad.radian -= 10;
+		}
+	}
+
+	if (yMove == TRUE) {
+		if (yDir == 1)
+			rad.radian += 1;
+		else
+			rad.radian -= 1;
+	}
+
+	if (zMove == TRUE) {
+		if (zDir == 2) {
+			if (sp.z < 160)
+				sp.z += 1;
+			rad.radian += 10;
+		}
+		
+		else if (zDir == 1) {
+			if (sp.z > -160)
+				sp.z -= 1;
+			rad.radian -= 10;
+		}
+	}
+
+	if (camera.xBOOL == TRUE) {
+		if (camera.xDir == 1)
+			camera.xRad = (camera.xRad + 1) % 360;
+		else if (camera.xDir == 2)
+			camera.xRad = (camera.xRad - 1) % 360;
+	}
+
+	if (camera.yBOOL == TRUE) {
+		if (camera.yDir == 1)
+			camera.yRad = (camera.yRad + 1) % 360;
+		else if (camera.yDir == 2)
+			camera.yRad = (camera.yRad - 1) % 360;
+	}
+
+	if (camera.zBOOL == TRUE) {
+		if (camera.zDir == 1)
+			camera.zRad = (camera.zRad + 1) % 360;
+		else if (camera.zDir == 2)
+			camera.zRad = (camera.zRad - 1) % 360;
+	}
 	glutTimerFunc(100, TimerFunction, 1);
 
 }
@@ -175,45 +291,157 @@ void Keyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
 	case 'x':
-		camera.x += 4;
+		if (camera.xBOOL == FALSE) {
+			camera.xBOOL = TRUE;
+			camera.xDir = 1;
+		}
+		else if (camera.xBOOL == TRUE)
+			camera.xBOOL = FALSE;
 		break;
+
 	case 'X':
-		camera.x -= 4;
+		if (camera.xBOOL == FALSE) {
+			camera.xBOOL = TRUE;
+			camera.xDir = 2;
+		}
+		else if (camera.xBOOL == TRUE)
+			camera.xBOOL = FALSE;
 		break;
+
 	case 'y':
-		camera.y += 4;
+		if (camera.yBOOL == FALSE) {
+			camera.yBOOL = TRUE;
+			camera.yDir = 1;
+		}
+		else if (camera.yBOOL == TRUE)
+			camera.yBOOL = FALSE;
 		break;
+
 	case 'Y':
-		camera.y -= 4;
+		if (camera.yBOOL == FALSE) {
+			camera.yBOOL = TRUE;
+			camera.yDir = 2;
+		}
+		else if (camera.yBOOL == TRUE)
+			camera.yBOOL = FALSE;
 		break;
+
 	case 'z':
-		camera.z += 4;
+		if (camera.zBOOL == FALSE) {
+			camera.zBOOL = TRUE;
+			camera.zDir = 1;
+		}
+		else if (camera.zBOOL == TRUE)
+			camera.zBOOL = FALSE;
 		break;
+
 	case 'Z':
-		camera.z -= 4;
-		break;
-	case '1':
-		shape = 1;
-		break;
-
-	case '2':
-		shape = 2;
+		if (camera.zBOOL == FALSE) {
+			camera.zBOOL = TRUE;
+			camera.zDir = 2;
+		}
+		else if (camera.zBOOL == TRUE)
+			camera.zBOOL = FALSE;
 		break;
 
-	case '3':
-		shape = 3;
+	case 'l':
+		if (xMove == FALSE) {
+			xMove = TRUE;
+			xDir = 1;
+		}
+		else
+			xMove = FALSE;
+
 		break;
 
-	case '4':
-		shape = 4;
+	case 'L':
+		if (xMove == FALSE) {
+			xMove = TRUE;
+			xDir = 2;
+		}
+		else
+			xMove = FALSE;
+
+		break;
+
+	case 'm':
+		if (yMove == FALSE) {
+			yMove = TRUE;
+			yDir = 1;
+		}
+		else
+			yMove = FALSE;
+		break;
+
+	case 'M':
+		if (yMove == FALSE) {
+			yMove = TRUE;
+			yDir = 2;
+		}
+		else
+			yMove = FALSE;
+
+		break;
+
+	case 'n':
+		if (zMove == FALSE) {
+			zMove = TRUE;
+			zDir = 1;
+		}
+		else
+			zMove = FALSE;
+		break;
+
+	case 'N':
+		if (zMove == FALSE) {
+			zMove = TRUE;
+			zDir = 2;
+		}
+		else
+			zMove = FALSE;
+		break;
+
+
+	case 'w':
+		camera.y += 5;
+		break;
+
+	case 'a':
+		camera.x -= 5;
+		break;
+
+	case 's':
+		camera.y -= 5;
+		break;
+
+	case 'd':
+		camera.x += 5;
+		break;
+
+	case '+':
+		camera.z += 5;
+		break;
+
+	case '-':
+		camera.z -= 5;
 		break;
 
 	case 'i':
+		camera.xRad = -30;
+		camera.yRad = 0;
+		camera.zRad = 0;
 
-		break;
+		camera.x = 0;
+		camera.y = 0;
+		camera.z = 0;
 
-	case 'q':
-		PostQuitMessage(0);
+		camera.xBOOL = FALSE;
+		camera.yBOOL = FALSE;
+		camera.zBOOL = FALSE;
+
+		camera.xDir = 0;
+		camera.yDir = 0;
+		camera.zDir = 0;
 		break;
 	}
 }
