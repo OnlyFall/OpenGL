23147916b623ff8064d0ae0e10599858ceb06ece
OnlyFall <34495911+OnlyFall@users.noreply.github.com>
2018-11-08 00:12:35 +0900

    30번 완성

---
+++ Chapter - 1/Project8/1 - 30.cpp
@@ -0,0 +1,478 @@
+#include <gl/freeglut.h>
+#include <stdlib.h>
+#include <random>
+#include <stdio.h>
+#include <gl\freeglut.h>
+#include <math.h>
+GLvoid drawScene(GLvoid);
+void TimerFunction(int value);
+void Keyboard(unsigned char key, int x, int y);
+GLvoid Reshape(int w, int h);
+void Mouse(int button, int state, int x, int y);
+void SpecialKeys(int key, int x, int y);
+static int mode = 1;
+
+static float cycleRad = 200;
+
+
+struct point_3 {
+	float x = 0;
+	float y = 450;
+	float z = 0;
+};
+
+#define pi 3.14
+//#define DEGTORAD pi/360 * 2
+#define DEGTORAD pi/180
+#define pointArrSize 20
+#define ShapeSize 15
+
+static point_3 spherePos;
+static BOOL start = FALSE;
+static BOOL bCrash = FALSE;
+static BOOL goaway = FALSE;
+
+GLdouble result[16] = { 0 };
+GLdouble Identity[16] = {
+	1.0, 0.0, 0.0, 0.0,
+	0.0, 1.0, 0.0, 0.0,
+	0.0, 0.0, 1.0, 0.0,
+	0.0, 0.0, 0.0, 1.0
+};
+GLdouble rotateX_P3[16] = {
+	1.0, 0.0, 0.0, 0.0,
+	0.0, cos(3 * DEGTORAD), -sin(3 * DEGTORAD), 0.0,
+	0.0, sin(3 * DEGTORAD), cos(3 * DEGTORAD), 0.0,
+	0.0, 0.0, 0.0, 1.0
+};
+GLdouble rotateX_M3[16] = {
+	1.0, 0.0, 0.0, 0.0,
+	0.0, cos(-3 * DEGTORAD), -sin(-3 * DEGTORAD), 0.0,
+	0.0, sin(-3 * DEGTORAD), cos(-3 * DEGTORAD), 0.0,
+	0.0, 0.0, 0.0, 1.0
+};
+GLdouble rotateY_P3[16] = {
+	cos(3 * DEGTORAD), 0.0, sin(3 * DEGTORAD), 0.0,
+	0.0, 1.0, 0.0, 0.0,
+	-sin(3 * DEGTORAD), 0.0, cos(3 * DEGTORAD), 0.0,
+	0.0, 0.0, 0.0, 1.0
+};
+GLdouble rotateY_M3[16] = {
+	cos(-3 * DEGTORAD), 0.0, sin(-3 * DEGTORAD), 0.0,
+	0.0, 1.0, 0.0, 0.0,
+	-sin(-3 * DEGTORAD), 0.0, cos(-3 * DEGTORAD), 0.0,
+	0.0, 0.0, 0.0, 1.0
+};
+GLdouble rotateZ_P3[16] = {
+	cos(3 * DEGTORAD), -sin(3 * DEGTORAD), 0.0, 0.0,
+	sin(3 * DEGTORAD), cos(3 * DEGTORAD), 0.0, 0.0,
+	0.0, 0.0, 1.0, 0.0,
+	0.0, 0.0, 0.0, 1.0
+};
+GLdouble rotateZ_M3[16] = {
+	cos(-3 * DEGTORAD), -sin(-3 * DEGTORAD), 0.0, 0.0,
+	sin(-3 * DEGTORAD), cos(-3 * DEGTORAD), 0.0, 0.0,
+	0.0, 0.0, 1.0, 0.0,
+	0.0, 0.0, 0.0, 1.0
+};
+
+GLvoid MultiMatrix(GLdouble y[16]) {
+	for (int i = 0; i < 16; i++) {
+		result[i] = 0;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			result[j + i * 4] = Identity[0 + i * 4] * y[0 + j] +
+				Identity[1 + i * 4] * y[4 + j] +
+				Identity[2 + i * 4] * y[8 + j] +
+				Identity[3 + i * 4] * y[12 + j];
+		}
+	}
+
+	for (int i = 0; i < 16; i++) {
+		Identity[i] = result[i];
+	}
+}
+GLfloat ctrlpoints[3][3][3] = { { { -200, 0, -200 },{ 0, 0, -200 },{ 200, 0, -200 } },{ { -200, 0, 0 },{ 0,350,0 },{ 200, 0, 0 } },{ { -200,0,200 },{ 0,0,200 },{ 200,0,200 } } };
+
+
+void reset()
+{
+	start = FALSE;
+	bCrash = FALSE;
+	goaway = FALSE;
+	cycleRad = 200;
+	spherePos.x = 0;
+	spherePos.z = 0;
+	spherePos.y = 300;
+}
+
+struct DOUBLE_3D_POS
+{
+	double x, y, z;
+};
+
+class Cam
+{
+private:
+	DOUBLE_3D_POS m_Eye = {};
+	DOUBLE_3D_POS m_Center = {};
+	DOUBLE_3D_POS m_Up = {};
+	DOUBLE_3D_POS m_MoveEye = {};
+	DOUBLE_3D_POS m_Angle = {};
+
+public:
+	Cam() // 생성자
+	{
+		m_Eye = { 0.0, 0.0, 0.0 };
+		m_Center = { 0.0, 0.0, -100.0 };
+		m_Up = { 0.0, 1.0, 0.0 };
+		m_MoveEye = { 0.0, 0.0, 0.0 };
+		m_Angle = { 0.0, 0.0, 0.0 };
+	}
+	void Init() // 초기화 함수
+	{
+		m_Eye = { 0.0, 0.0, 0.0 };
+		m_Center = { 0.0, 0.0, -100.0 };
+		m_Up = { 0.0, 1.0, 0.0 };
+		m_MoveEye = { 0.0, 0.0, 0.0 };
+		m_Angle = { 0.0, 0.0, 0.0 };
+	}
+
+	void drawCamera() // 카메라를 배치하는 함수
+	{
+		//glPushMatrix();
+		//glLoadIdentity(); // 현재의 변환 행렬을 단위행렬로 초기화 - 시각 좌표계를 원점으로 초기화
+		// 관측 변환: 카메라의 위치 설정 (필요한 경우, 다른 곳에 설정 가능)
+		gluLookAt(m_Eye.x, m_Eye.y, m_Eye.z, m_Center.x, m_Center.y, m_Center.z, m_Up.x, m_Up.y, m_Up.z);
+
+		glRotated(-m_Angle.x, 0.0, 1.0, 0.0);
+		glRotated(m_Angle.y, 1.0, 0.0, 0.0);
+		glRotated(m_Angle.z, 0.0, 0.0, 1.0);
+		glTranslated(-m_MoveEye.x, -m_MoveEye.y, m_MoveEye.z);
+		//glPopMatrix();
+	}
+
+public:
+	void rotateEye(double x, double y, double z) // 카메라 회전 함수
+	{
+		m_Angle.x += x;
+		m_Angle.y += y;
+		m_Angle.z += z;
+	}
+	void moveEye(double x, double y, double z) // 카메라 이동 함수
+	{
+		m_MoveEye.x += x;
+		m_MoveEye.y += y;
+		m_MoveEye.z += z;
+	}
+};
+
+static int count = 0;
+static Cam camera;
+void Mouse(int button, int state, int x, int y);
+
+
+
+void SetupRC()
+{
+	camera.rotateEye(0, 0, 0);
+	spherePos.y = 300;
+
+}
+
+void main(int agrc, char *argv[]) { // 윈도우 초기화 및 생성 
+	glutInit(&agrc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	// 더블 버퍼링과 3차원으로 그린다. 
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Points Drawing");
+	// 상태 변수 초기화 함수 
+	SetupRC();
+	// 필요한 콜백 함수 설정
+	glutMouseFunc(Mouse);
+	glutDisplayFunc(drawScene); // 출력 콜백 함수 
+	glutReshapeFunc(Reshape); // 다시 그리기 콜백 함수
+	glutKeyboardFunc(Keyboard); // 키보드 입력 콜백 함수
+	glutTimerFunc(100, TimerFunction, 1); // 타이머 콜백 함수
+	glutSpecialFunc(SpecialKeys);
+	glutMainLoop(); // 이벤트 루프 실행하기
+}
+
+GLvoid drawPlan(GLvoid)
+{
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	{
+		glPushMatrix();
+		//	glRotatef(30, 1, 0, 0);
+		glScalef(1, 0.01, 1);
+		glutSolidCube(400);
+		glPopMatrix();
+	}
+}
+
+GLvoid drawPyramid(float x, float z)
+{
+	glPushMatrix();
+	glTranslatef(x, 0, z);
+
+	glPushMatrix();
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glBegin(GL_POLYGON);
+	glVertex3f(-25, 5, 25);
+	glVertex3f(-25, 5, -25);
+	glVertex3f(25, 5, -25);
+	glVertex3f(25, 5, 25);
+	glEnd();
+	glPopMatrix();
+
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glPushMatrix();
+	
+	glBegin(GL_POLYGON);
+	glVertex3f(25, 5, -25);
+	glVertex3f(25, 5, 25);
+	glVertex3f(0, 50, 0);
+	glEnd();
+
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glBegin(GL_POLYGON);
+	glVertex3f(-25, 5, 25);
+	glVertex3f(-25, 5, -25);
+	glVertex3f(0, 50, 0);
+	glEnd();
+
+	glColor3f(1.0f, 0.0f, 0.7f);
+	glBegin(GL_POLYGON);
+	glVertex3f(25, 5, -25);
+	glVertex3f(-25, 5, -25);
+	glVertex3f(0, 50, 0);
+	glEnd();
+
+	glColor3f(1.0f, 0.0f, 0.5f);
+	glBegin(GL_POLYGON);
+	glVertex3f(-25, 5, 25);
+	glVertex3f(25, 5, 25);
+	glVertex3f(0, 50, 0);
+	glEnd();
+	glPopMatrix();
+
+	glPopMatrix();
+}
+
+GLvoid DrawSphere()
+{
+	glPushMatrix();
+
+	//Move
+	glTranslatef(spherePos.x, spherePos.y, spherePos.z);
+	//Rotation 행렬 곱해주기.
+	glMultMatrixd(Identity);
+
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	glutWireSphere(20, 20, 20);
+
+	glColor4f(0.0f, 0.0f, 0.0f, 1.0f);
+	glutSolidSphere(20, 10, 10);
+	glPopMatrix();
+}
+
+
+static BOOL check = FALSE;
+GLvoid drawScene(GLvoid)
+{
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+
+
+	glPushMatrix();
+	camera.drawCamera();
+
+	drawPlan();
+	
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glMap2f(GL_MAP2_VERTEX_3, 0.0, 1.0, 3, 3, 0.0, 1.0, 9, 3,&ctrlpoints[0][0][0]);
+	glEnable(GL_MAP2_VERTEX_3);
+
+	glMapGrid2f(10, 0.0, 1.0, 10, 0.0, 1.0);
+	glEvalMesh2(GL_FILL, 0, 10, 0, 10);
+	glDisable(GL_MAP2_VERTEX_3);
+
+	drawPyramid(-150, -150);
+	drawPyramid(150, 150);
+	drawPyramid(150, -150);
+	drawPyramid(-150, 150);
+
+	DrawSphere();
+
+	glPopMatrix();
+	glutSwapBuffers();
+	//	glFlush(); // 화면에 출력하기 
+}
+
+
+
+static float zZoom = 45;
+GLvoid Reshape(int w, int h)
+{
+	GLfloat nRange = 800.0f;
+
+	glViewport(-1, -1, w, h);  // 투영 공간을 화면 안쪽으로 이동하여 시야를 확보한다.
+
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (mode == 1) {
+		//-- 투영은 직각 투영 또는 원근 투영 중 한 개를 설정한다. // 1. 클리핑 공간 설정: 원근 투영인 경우
+		gluPerspective(zZoom, 1.0, 1.0, 1000.0);
+		glTranslatef(0.0, 0.0, -500.0);     // 투영 공간을 화면 안쪽으로 이동하여 시야를 확보한다.
+	}
+	else if (mode == 2)
+		glOrtho(-400.0, 400.0, -300.0, 300.0, -400.0, 400.0);
+
+	glMatrixMode(GL_MODELVIEW);
+
+	glLoadIdentity();
+}
+
+static int sphereRad = 0;
+
+void TimerFunction(int value)
+{
+	glutPostRedisplay();
+
+	if (start == TRUE)
+		cycleRad -= 0.5;
+
+	if (cycleRad > 0) {
+		spherePos.x = cos((3.141592 / 180.f) * sphereRad) * cycleRad;
+		spherePos.z = sin((3.141592 / 180.f) * sphereRad) * cycleRad;
+		sphereRad = (sphereRad + 1) % 360;
+	}
+
+	else {
+		if(spherePos.y >= 150 && bCrash == FALSE && goaway == FALSE)
+			spherePos.y -= 1;
+		
+		else if (spherePos.y < 150 && bCrash == FALSE && goaway == FALSE) {
+			spherePos.y -= 1;
+			ctrlpoints[1][1][1] -= 1;
+
+			if (spherePos.y < 100)
+				bCrash = TRUE;
+		}
+		else if (bCrash == TRUE && goaway == FALSE) {
+			spherePos.y += 1;
+			ctrlpoints[1][1][1] += 1;
+			if (spherePos.y >= 130)
+				goaway = TRUE;
+		}
+		else if(goaway == TRUE){
+			spherePos.x -= 1;
+			if(spherePos.y > 0)
+				spherePos.y -= 0.5;
+			MultiMatrix(rotateZ_M3);
+		}
+	}
+
+	glutTimerFunc(1, TimerFunction, 100);
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 'k':
+		if (mode == 1) {
+			mode = 2;
+			Reshape(800, 600);
+		}
+		else if (mode == 2) {
+			mode = 1;
+			Reshape(800, 600);
+		}
+		break;
+
+	case 'r':
+	case 'R':
+		reset();
+		break;
+
+	case 'x':
+		camera.rotateEye(1, 0, 0);
+		break;
+	case 'X':
+		camera.rotateEye(-1, 0, 0);
+		break;
+
+	case 'y':
+		camera.rotateEye(0, 1, 0);
+		break;
+	case 'Y':
+		camera.rotateEye(0, -1, 0);
+		break;
+
+	case 'z':
+		camera.rotateEye(0, 0, 1);
+		break;
+	case 'Z':
+		camera.rotateEye(0, 0, -1);
+		break;
+
+		//move
+	case 'w':
+		camera.moveEye(0, 1, 0);
+		break;
+	case 'a':
+		camera.moveEye(-1, 0, 0);
+		break;
+
+	case 's':
+		camera.moveEye(0, -1, 0);
+		break;
+
+	case 'd':
+		camera.moveEye(1, 0, 0);
+		break;
+	case '+':
+		camera.moveEye(0, 0, 1);
+		break;
+	case '-':
+		camera.moveEye(0, 0, -1);
+		break;
+	case 'i':
+		camera.Init();
+		break;
+
+	case '9':
+		zZoom += 1;
+		Reshape(800, 600);
+		break;
+
+	case '0':
+		zZoom -= 1;
+		Reshape(800, 600);
+		break;
+
+	case 'q':
+		PostQuitMessage(0);
+		break;
+	}
+}
+
+
+
+static int swi = 0;
+void Mouse(int button, int state, int x, int y)
+{
+
+	glutPostRedisplay();
+}
+
+void SpecialKeys(int key, int x, int y) {
+	if (key == GLUT_KEY_SHIFT_L)
+		start = TRUE;
+	glutPostRedisplay();
+}
